f7bf22586bbf8dc01bd8786ad493eba116115e17
Vyacheslav Tulaev <hammaren23@gmail.com>
2022-09-17 16:37:25 +0600

    sdf

--- index.php
+++ index.php
@@ -13,6 +13,16 @@
    Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты. Даже путь возвращайся продолжил меня своих рыбными. Переписывается рукописи приставка заглавных ее злых коварный строчка рыбными, щеке свой диких текста!
 </div>
 
+<img src="data:text/html;charset=utf-8,%3C!DOCTYPE%20HTML%20PUBLIC%20%22-' +
+    '%2F%2FW3C%2F%2FDTD%20HTML%204.0%2F%2FEN%22%3E%0D%0A%3Chtml%20lang%3D%22en' +
+    '%22%3E%0D%0A%3Chead%3E%3Ctitle%3EEmbedded%20Window%3C%2Ftitle%3E%3C%2Fhea' +
+    'd%3E%0D%0A%3Cbody%3E%3Ch1%3E42%3C%2Fh1%3E%3C%2Fbody%3E%0D%0A%3C%2Fhtml%3E' +
+    '%0D%0A" alt="Larry" />
+
+
+
+
+
 
 <form action="" method="get" id="loginForm">
    <label>
@@ -38,6 +48,8 @@
    <script>
       const btn = document.querySelector('#btn');
       const form = document.getElementById('loginForm');
+
+
 
 
       // form.onsubmit = function(event) {
